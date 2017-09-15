6655ebd1741f950cea7bca14fcd613ccc2693823
funasaka koichi <funasakakui@gmail.com>
2017-09-16 00:38:54 +0900

    mod hook script

--- hooks/before_install.sh
+++ hooks/before_install.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
  
-app_src_path="/usr/src/app"
-app_log_path="/var/log/app"
- 
-mkdir "$app_src_path" "$app_log_path"
+test -f /var/www/html/index.html && rm -f /var/www/html/index.html
